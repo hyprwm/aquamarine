f6d4c5e6a3adb78819b0256c4037296fcb84d4dc
Vaxry <vaxry@vaxry.net>
2024-06-26 22:25:45 +0200

    drm: prettify bitwise ops

--- src/backend/drm/DRM.cpp
+++ src/backend/drm/DRM.cpp
@@ -977,12 +977,12 @@ bool Aquamarine::CDRMOutput::commitState(bool onlyTest) {
         return false;
     }
 
-    if (COMMITTED & COutputState::eOutputStateProperties::AQ_OUTPUT_STATE_BUFFER && !STATE.buffer) {
+    if ((COMMITTED & COutputState::eOutputStateProperties::AQ_OUTPUT_STATE_BUFFER) && !STATE.buffer) {
         backend->backend->log(AQ_LOG_ERROR, "drm: No buffer committed");
         return false;
     }
 
-    if (COMMITTED & COutputState::eOutputStateProperties::AQ_OUTPUT_STATE_BUFFER && STATE.buffer->attachments.has(AQ_ATTACHMENT_DRM_KMS_UNIMPORTABLE)) {
+    if ((COMMITTED & COutputState::eOutputStateProperties::AQ_OUTPUT_STATE_BUFFER) && STATE.buffer->attachments.has(AQ_ATTACHMENT_DRM_KMS_UNIMPORTABLE)) {
         backend->backend->log(AQ_LOG_TRACE, "drm: Cannot commit a KMS-unimportable buffer.");
         return false;
     }
