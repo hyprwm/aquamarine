19c19d36e9615b7857fa0ec3d486ab4bd1a882d5
Mihai Fufezan <mihai@fufexan.net>
2024-07-01 10:52:16 +0300

    Nix: init

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "A very light linux rendering backend library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+
+    hyprutils = {
+      url = "github:hyprwm/hyprutils";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
+    };
+
+    hyprwayland-scanner = {
+      url = "github:hyprwm/hyprwayland-scanner";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.systems.follows = "systems";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    ...
+  } @ inputs: let
+    inherit (nixpkgs) lib;
+    eachSystem = lib.genAttrs (import systems);
+    pkgsFor = eachSystem (system:
+      import nixpkgs {
+        localSystem.system = system;
+        overlays = with self.overlays; [aquamarine];
+      });
+    mkDate = longDate: (lib.concatStringsSep "-" [
+      (builtins.substring 0 4 longDate)
+      (builtins.substring 4 2 longDate)
+      (builtins.substring 6 2 longDate)
+    ]);
+  in {
+    overlays = {
+      default = self.overlays.aquamarine;
+      aquamarine = lib.composeManyExtensions [
+        inputs.hyprutils.overlays.default
+        inputs.hyprwayland-scanner.overlays.default
+        (final: prev: {
+          aquamarine = final.callPackage ./nix/default.nix {
+            stdenv = final.gcc13Stdenv;
+            version = "0.pre" + "+date=" + (mkDate (self.lastModifiedDate or "19700101")) + "_" + (self.shortRev or "dirty");
+          };
+          aquamarine-with-tests = final.aquamarine.override {doCheck = true;};
+        })
+      ];
+    };
+
+    packages = eachSystem (system: {
+      default = self.packages.${system}.aquamarine;
+      inherit (pkgsFor.${system}) aquamarine aquamarine-with-tests;
+    });
+
+    formatter = eachSystem (system: pkgsFor.${system}.alejandra);
+  };
+}
---
+++ nix/default.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  stdenv,
+  cmake,
+  hyprutils,
+  hyprwayland-scanner,
+  libdisplay-info,
+  libdrm,
+  libffi,
+  libinput,
+  libseat,
+  mesa,
+  pixman,
+  pkg-config,
+  udev,
+  wayland,
+  wayland-protocols,
+  version ? "git",
+  doCheck ? false,
+}:
+stdenv.mkDerivation {
+  pname = "aquamarine";
+  inherit version doCheck;
+  src = ../.;
+
+  nativeBuildInputs = [
+    cmake
+    hyprwayland-scanner
+    pkg-config
+  ];
+
+  buildInputs = [
+    hyprutils
+    libdisplay-info
+    libdrm
+    libffi
+    libinput
+    libseat
+    mesa
+    pixman
+    udev
+    wayland
+    wayland-protocols
+  ];
+
+  outputs = ["out" "dev"];
+
+  cmakeBuildType = "RelWithDebInfo";
+
+  dontStrip = true;
+
+  meta = {
+    homepage = "https://github.com/hyprwm/aquamarine";
+    description = "A very light linux rendering backend library";
+    license = lib.licenses.bsd3;
+    platforms = lib.platforms.linux;
+  };
+}
