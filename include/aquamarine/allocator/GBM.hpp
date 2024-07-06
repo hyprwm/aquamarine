cb917fec527681b13adcc756e505afefdd875e8d
Tom Englund <tomenglund26@gmail.com>
2024-07-06 07:34:40 +0200

    allocator: add missing virtual destructor
    add missing virtual destructor to base class and free gbm device on
    destruction of CGBMAllocator

--- include/aquamarine/allocator/GBM.hpp
+++ include/aquamarine/allocator/GBM.hpp
@@ -35,6 +35,7 @@ namespace Aquamarine {
 
     class CGBMAllocator : public IAllocator {
       public:
+        ~CGBMAllocator();
         static Hyprutils::Memory::CSharedPointer<CGBMAllocator> create(int drmfd_, Hyprutils::Memory::CWeakPointer<CBackend> backend_);
 
         virtual Hyprutils::Memory::CSharedPointer<IBuffer>      acquire(const SAllocatorBufferParams& params, Hyprutils::Memory::CSharedPointer<CSwapchain> swapchain_);
