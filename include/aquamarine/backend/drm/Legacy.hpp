7a84686b4a6dbf7df0bfb4856e6299f995c50d15
UjinT34 <41110182+UjinT34@users.noreply.github.com>
2024-07-20 21:56:18 +0300

    output: remove redundant needsFrame, allow skip on mouse event (#21)

--- include/aquamarine/backend/drm/Legacy.hpp
+++ include/aquamarine/backend/drm/Legacy.hpp
@@ -8,12 +8,11 @@ namespace Aquamarine {
         CDRMLegacyImpl(Hyprutils::Memory::CSharedPointer<CDRMBackend> backend_);
         virtual bool commit(Hyprutils::Memory::CSharedPointer<SDRMConnector> connector, SDRMConnectorCommitData& data);
         virtual bool reset();
-        virtual bool moveCursor(Hyprutils::Memory::CSharedPointer<SDRMConnector> connector);
+        virtual bool moveCursor(Hyprutils::Memory::CSharedPointer<SDRMConnector> connector, bool skipShedule = false);
 
       private:
-
-        bool commitInternal(Hyprutils::Memory::CSharedPointer<SDRMConnector> connector, SDRMConnectorCommitData& data);
-        bool testInternal(Hyprutils::Memory::CSharedPointer<SDRMConnector> connector, SDRMConnectorCommitData& data);
+        bool                                         commitInternal(Hyprutils::Memory::CSharedPointer<SDRMConnector> connector, SDRMConnectorCommitData& data);
+        bool                                         testInternal(Hyprutils::Memory::CSharedPointer<SDRMConnector> connector, SDRMConnectorCommitData& data);
 
         Hyprutils::Memory::CWeakPointer<CDRMBackend> backend;
     };
