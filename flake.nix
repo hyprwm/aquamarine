0aba5246b516dbede58871bb2f73e0faaac1fefb
Mihai Fufezan <mihai@fufexan.net>
2026-03-02 16:09:04 +0200

    nix: separate overlay with deps

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
         system:
         import nixpkgs {
           localSystem.system = system;
-          overlays = with self.overlays; [ aquamarine ];
+          overlays = with self.overlays; [ aquamarine-with-deps ];
         }
       );
       pkgsCrossFor = eachSystem (
@@ -40,39 +40,12 @@
         import nixpkgs {
           localSystem = system;
           crossSystem = crossSystem;
-          overlays = with self.overlays; [ aquamarine ];
+          overlays = with self.overlays; [ aquamarine-with-deps ];
         }
       );
-      mkDate =
-        longDate:
-        (lib.concatStringsSep "-" [
-          (builtins.substring 0 4 longDate)
-          (builtins.substring 4 2 longDate)
-          (builtins.substring 6 2 longDate)
-        ]);
-      version = lib.removeSuffix "\n" (builtins.readFile ./VERSION);
     in
     {
-      overlays = {
-        default = self.overlays.aquamarine;
-
-        aquamarine = lib.composeManyExtensions [
-          inputs.hyprutils.overlays.default
-          inputs.hyprwayland-scanner.overlays.default
-          (final: prev: {
-            aquamarine = final.callPackage ./nix/default.nix {
-              stdenv = final.gcc15Stdenv;
-              version =
-                version
-                + "+date="
-                + (mkDate (self.lastModifiedDate or "19700101"))
-                + "_"
-                + (self.shortRev or "dirty");
-            };
-            aquamarine-with-tests = final.aquamarine.override { doCheck = true; };
-          })
-        ];
-      };
+      overlays = import ./nix/overlays.nix { inherit lib self inputs; };
 
       packages = eachSystem (system: {
         default = self.packages.${system}.aquamarine;
