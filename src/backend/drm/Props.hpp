316ae208f225c0fb56f4d9b5bfaae350fc140b08
UjinT34 <ujint34@mail.ru>
2026-04-09 20:52:16 +0300

    full range

--- src/backend/drm/Props.hpp
+++ src/backend/drm/Props.hpp
@@ -5,6 +5,7 @@
 namespace Aquamarine {
     bool  getDRMConnectorProps(int fd, uint32_t id, SDRMConnector::UDRMConnectorProps* out);
     bool  getDRMConnectorColorspace(int fd, uint32_t id, SDRMConnector::UDRMConnectorColorspace* out);
+    bool  getDRMConnectorBroadcastRGB(int fd, uint32_t id, SDRMConnector::UDRMConnectorBroadcastRGB* out);
     bool  getDRMCRTCProps(int fd, uint32_t id, SDRMCRTC::UDRMCRTCProps* out);
     bool  getDRMPlaneProps(int fd, uint32_t id, SDRMPlane::UDRMPlaneProps* out);
     bool  getDRMPlaneColorRange(int fd, uint32_t id, SDRMPlane::UDRMPlaneColorRange* out);
